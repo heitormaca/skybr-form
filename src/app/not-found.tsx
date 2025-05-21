8004554292b915c297816fcb2cd3a52ab9af3bf2
Heitor Soares <heitor_soares@live.com>
2025-05-21 00:42:48 -0300

    fix: 404 page layout;

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 export default function NotFound() {
   return (
-    <Center w="100%" h="100vh">
+    <Center w="100%" h="100dvh">
       <Stack gap="lg">
         <Stack gap={0}>
           <Title ta="center" fz="7rem" lh="7rem" order={1}>
