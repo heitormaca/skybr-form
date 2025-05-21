3ef01029352eb1d8fbc5d9ddc5245eefb87b87b6
Heitor Soares <heitor_soares@live.com>
2025-05-21 01:29:25 -0300

    feat: add title, description and image to all pages

--- src/app/0800/page.tsx
+++ src/app/0800/page.tsx
@@ -1,4 +1,11 @@
+import { Metadata } from 'next'
+
 import Zero800 from '@/components/pages/0800'
+
+export const metadata: Metadata = {
+  title: '0800',
+  description: 'O programa de 0800 do clã SkyBR',
+}
 
 export default function Zero800Page() {
   return (
